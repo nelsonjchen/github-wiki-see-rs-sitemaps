ebf1f31c4c3cb2026e67ef11ae1c389c9b993e36
Nelson Chen <crazysim@gmail.com>
2021-06-05 15:28:31 -0700

    Initial query against project

--- generator/generate.py
+++ generator/generate.py
@@ -1,4 +1,4 @@
-query = '''
+payload_query = '''
 #standardSQL
 CREATE TEMPORARY FUNCTION
   parsePayload(payload STRING)
@@ -13,11 +13,20 @@ WITH
   FROM
     `github-wiki-see.scratch.multi_page`)
 SELECT
-  DISTINCT html_urls
+  DISTINCT html_url
 FROM
   parsed_payloads
 CROSS JOIN
-  UNNEST(parsed_payloads.html_urls) AS html_urls
+  UNNEST(parsed_payloads.html_urls) AS html_url
 '''
 
-print("Hello")
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+# Perform a query.
+query_job = client.query(payload_query)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print(row.html_url)
