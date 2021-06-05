11cc62f4318c63b9981bb3efb93710329897c33a
Nelson Chen <crazysim@gmail.com>
2021-06-05 15:33:55 -0700

    functionalize

--- generator/generate.py
+++ generator/generate.py
@@ -1,32 +1,40 @@
-payload_query = '''
-#standardSQL
-CREATE TEMPORARY FUNCTION
-  parsePayload(payload STRING)
-  RETURNS ARRAY<STRING>
-  LANGUAGE js AS """ try { return JSON.parse(payload).pages.reduce((a,
-      s) => {a.push(s.html_url); return a},
-    []); } catch (e) { return []; } """;
-WITH
-  parsed_payloads AS (
-  SELECT
-    parsePayload(payload) AS html_urls
-  FROM
-    `github-wiki-see.scratch.multi_page`)
-SELECT
-  DISTINCT html_url
-FROM
-  parsed_payloads
-CROSS JOIN
-  UNNEST(parsed_payloads.html_urls) AS html_url
-'''
-
+# noinspection PyPackageRequirements
 from google.cloud import bigquery
 
-client = bigquery.Client()
 
-# Perform a query.
-query_job = client.query(payload_query)  # API request
-rows = query_job.result()  # Waits for query to finish
+def generate():
+    table = 'github-wiki-see.scratch.multi_page'
+    payload_query = ('\n'
+                     ' #standardSQL\n'
+                     'CREATE TEMPORARY FUNCTION\n'
+                     '  parsePayload(payload STRING)\n'
+                     '  RETURNS ARRAY<STRING>\n'
+                     '  LANGUAGE js AS """ try { return JSON.parse(payload).pages.reduce((a,\n'
+                     '      s) => {a.push(s.html_url); return a},\n'
+                     '    []); } catch (e) { return []; } """;\n'
+                     'WITH\n'
+                     '  parsed_payloads AS (\n'
+                     '  SELECT\n'
+                     '    parsePayload(payload) AS html_urls\n'
+                     '  FROM\n'
+                     f'    `{table}` )\n'
+                     'SELECT\n'
+                     '  DISTINCT html_url\n'
+                     'FROM\n'
+                     '  parsed_payloads\n'
+                     'CROSS JOIN\n'
+                     '  UNNEST(parsed_payloads.html_urls) AS html_url\n'
+                     )
 
-for row in rows:
-    print(row.html_url)
+    client = bigquery.Client()
+
+    # Perform a query.
+    query_job = client.query(payload_query)  # API request
+    rows = query_job.result()  # Waits for query to finish
+
+    for row in rows:
+        print(row.html_url)
+
+
+if __name__ == "__main__":
+    generate()
